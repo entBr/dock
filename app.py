a571d2694594ea6b8deae23c08227e8ce07b1335
entBr <brandonpro06@hotmail.com>
2024-10-03 20:55:19 -0600

    Actualizar programa 2

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-import argparse
-
-# Crear el parser de argumentos
-parser = argparse.ArgumentParser(description="Ingresa un número")
-parser.add_argument('numero', type=int, help="Número ingresado por el usuario")
-
-# Parsear los argumentos
-args = parser.parse_args()
-
-# Obtener el número ingresado
-numero = args.numero
-
-print(f"El número ingresado es: {numero}")
+print("HOLA")
